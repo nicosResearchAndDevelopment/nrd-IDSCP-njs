d3943b84f5d8f9ecebbd9da6c924344f8d67409e
Jörg Langkau <jlangkau@users.noreply.github.com>
2020-04-12 16:59:42 +0200

    initiating src (0.0.1)

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # nrd-isdscp-njs
+
 IDS Communication Protocol for node.js
+
+
+
+see also: [Trusted Connector, by Fraunhofer AISEC](https://industrial-data-space.github.io/trusted-connector-documentation/)
+
+---
---
+++ src/nrd-idscp-njs.js
@@ -0,0 +1,14 @@
+/*******************
+
+	description	: 
+	version   	: 0.0.1
+	see also 	:
+	author    	: jlangkau@nicos-rd.com
+	
+********************/
+	
+module.exports = {
+	'enum'   = null,
+	'server' = null,
+	'client' = null
+};
